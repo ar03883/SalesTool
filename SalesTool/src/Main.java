51e8fc74e6261346e4ba70df7cb4e444f514af38
Anslea <Anslea@DESKTOP-JLPT35D>
2016-11-06 17:07:07 -0500

    1st push

---
+++ SalesTool/src/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+
+   public static void main(String[] args) {
+       SalesData data = new SalesData();
+       displayGreeting();
+       data.display();
+   } //End main
+
+
+  private static void displayGreeting() {
+           System.out.println("HELLO HAPPY SALES PEOPLE!");
+           System.out.println("THIS APP SHOWS SALES DATA");
+           
+           System.out.println("Test1");;
+           
+  } //End displayGreeting
+
+}  //End class Main
