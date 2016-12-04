c2cfcf5b259f9ce3ddd2db1ed1506331e13de359
Anslea <Anslea@192.168.1.133>
2016-12-04 15:54:57 -0500

    Added a second test output

--- SalesTool/src/Main.java
+++ SalesTool/src/Main.java
@@ -13,7 +13,9 @@ public class Main {
            System.out.println("HELLO HAPPY SALES PEOPLE!");
            System.out.println("THIS APP SHOWS SALES DATA");
            
-           System.out.println("Test1");;
+           System.out.println("Test1");
+           System.out.println("Test2");
+           
            
   } //End displayGreeting
 
